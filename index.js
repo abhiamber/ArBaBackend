6f20f3038cb1423e2c84aebf17f6f9891f9196fa
abhiamber191 <abhiamber191@gmail.com>
2023-04-01 16:37:06 +0530

    done

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const CartRouter = require("./models/category.model");
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const cors = require("cors");
-const { urlencoded } = require("express");
-app.use(urlencoded({ extended: true }));
+// const { urlencoded } = require("express");
+// app.use(urlencoded({ extended: true }));
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));
